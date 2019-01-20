016b446ed9520fb3d6f28ccdc94378661b076e41
dylanalvarez1 <dylanalvarez1@ufl.edu>
2019-01-20 02:58:18 -0500

    initial commit for async problem

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
-# Alt-Facts
 
-Same news, different sites. A Firefox web extension
+# Inspiration
 
-## License
+###### What is the truth? Were we a team of naive yet enthusiastic individual, committed to improving the current state of politics and discourse in America? Or did our original plans fall through forcing us to come up with an idea that sounded decent and plausible given the limited time we had? The world may never know, but by using this extension you will be able to challenge your own biases and learn more about the world.
 
-This project is licensed under the GPLv3 License - see the [LICENSE.md](LICENSE.md) file for details.
+## What it does
+######The extension suggest similar news stories to the page your currently on from sites with different biases.
+
+## How we built it
+####### Using the standard WebExtensions API with HTML, CSS, and JavaScript.
+
+## Domain Competition
+######We have registered altfacts.info to point to the GitHub repository.
+
+## What's next for Alt-Facts
+######The code will be available on the Open Source Club repository as an easy project for members and others to learn about WebExtensions and improve on.
+
+####Try it out
+
+- altfacts.info
+- GitHub Repo
+
--- popup/popup.js
+++ popup/popup.js
@@ -1,21 +1,21 @@
- //News organizations that we will use to tell which link needs to be "recommended"
- const orgs = ["https://www.foxnews.com", "https://www.bbc.com", "http://www.msnbc.com"];
- let recommended = [];
- const dict = {
+//News organizations that we will use to tell which link needs to be "recommended"
+const orgs = ["https://www.foxnews.com", "https://www.bbc.com", "http://www.msnbc.com"];
+let recommended = [];
+const dict = {
     "https://www.foxnews.com": "images/fox.png",
     "https://www.bbc.com": "images/bbc.jpeg",
     "http://www.msnbc.com": "images/msnbc.png"
- }
+}
 
 //Query info to get active tabs
 let queryInfo = {
     "active": true,
 }
 
+console.log(gapi)
+
 //Get info from the current active browser using the queryInfo object
 let querying = browser.tabs.query(queryInfo).then((result) => {
-
-   
 
     //Search google with this title
     console.log("Title: ", result);
@@ -24,7 +24,21 @@ let querying = browser.tabs.query(queryInfo).then((result) => {
     //Figure out which news site this current article is on. Find the current domain, get the recommended domains, apply the links
     let currentSite = result[0].url.match(/^((?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+))/img, "")[0];
     console.log("title name: ", currentSite);
-    
+
+    //Disable website if not on supported organization
+    if(!orgs.includes(currentSite)) {
+        console.log("NOT SUPPORTED");
+        //Remove all children of popup html
+        let popup = document.getElementById("popup");
+        while(popup.hasChildNodes) {
+            popup.removeChild(popup.firstChild);
+        }
+        let error = document.createElement("div");
+        let errorMessage = document.createTextNode("Website not supported");
+        error.appendChild(errorMessage);
+        popup.append(error);
+    }
+   
     //Iterate through the differents orgs and push the non current domains into reccomended domains
     orgs.forEach(org => {
         if(org != currentSite) {
@@ -33,21 +47,56 @@ let querying = browser.tabs.query(queryInfo).then((result) => {
     });
 
     //TODO: GET GOOGLE SEARCH RESULTS HERE based on searchQuery (only use recommened domains)
+    
+    loadClient().then(() => {
+        execute(searchQuery);
 
-    //TODO: APPLY LINKS TO RECOMMENDED ARRAY BASED FROM THE RESULTS OF THE GOOGLE SEARCH
+        //TODO: APPLY LINKS TO RECOMMENDED ARRAY BASED FROM THE RESULTS OF THE GOOGLE SEARCH
 
-    //Match the images with links and display them in correct location
-    document.getElementById("current-news-link").href = result[0].url;
-    document.getElementById("current-news-image").src = dict[currentSite];
+        //Match the images with links and display them in correct location
+        document.getElementById("current-news-link").href = result[0].url;
+        document.getElementById("current-news-image").src = dict[currentSite];
 
-    //TODO: Replace links with accurate ones once google results are implemented
-    document.getElementById("recommended-news-first-link").href = result[0].url;
-    document.getElementById("recommended-news-first-image").src = dict[currentSite];
-    document.getElementById("recommended-news-second-link").href = result[0].url;
-    document.getElementById("recommended-news-second-image").src = dict[currentSite];
+        //TODO: Replace links with accurate ones once google results are implemented
+        document.getElementById("recommended-news-first-link").href = result[0].url;
+        document.getElementById("recommended-news-first-image").src = dict[currentSite];
+        document.getElementById("recommended-news-second-link").href = result[0].url;
+        document.getElementById("recommended-news-second-image").src = dict[currentSite];
+    });
+    
+    
+
+    
 
 
 
 });
+
+function loadClient() {
+    console.log("in loadClient")
+    let test = null;
+    let test2;
+    console.log(test)
+    console.log(test2)
+    console.log(gapi.load)
+    gapi.client.setApiKey(AIzaSyBwYfVMhEL8QPZ2RGfGYIhTE0LWH6sXGN4);
+    return gapi.client.load("https://content.googleapis.com/discovery/v1/apis/customsearch/v1/rest")
+        .then(function() { console.log("GAPI client loaded for API"); },
+            function(err) { console.error("Error loading GAPI client for API", err); });
+}
+
+// Make sure the client is loaded before calling this method.
+function execute(query) {
+	console.log("in execute")
+    return gapi.client.search.cse.list({
+        "q": query,
+        "cx": "004649689299812298879:wkbvmvrnja8"
+    })
+    .then(function(response) {
+        // Handle the results here (response.result has the parsed body).
+        console.log("Response", response);
+        },
+        function(err) { console.error("Execute error", err); });
+}
 
 console.log(`Alt-Facts: Was loaded!`);
