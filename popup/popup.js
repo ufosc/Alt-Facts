ba7baf00823d54bfada04e370bf64fc01ebbd5f0
dylanalvarez1 <dylanalvarez1@ufl.edu>
2019-01-20 00:27:38 -0500

    implement more logic for extension

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,11 @@
  //News organizations that we will use to tell which link needs to be "recommended"
  const orgs = ["https://www.foxnews.com", "https://www.bbc.com", "http://www.msnbc.com"];
+ let recommended = [];
+ const dict = {
+    "https://www.foxnews.com": "images/fox.png",
+    "https://www.bbc.com": "images/bbc.jpeg",
+    "http://www.msnbc.com": "images/msnbc.png"
+ }
 
 //Query info to get active tabs
 let queryInfo = {
@@ -18,7 +24,7 @@ let querying = browser.tabs.query(queryInfo).then((result) => {
     //Figure out which news site this current article is on. Find the current domain, get the recommended domains, apply the links
     let currentSite = result[0].url.match(/^((?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+))/img, "")[0];
     console.log("title name: ", currentSite);
-    let recomended = [];
+    
     //Iterate through the differents orgs and push the non current domains into reccomended domains
     orgs.forEach(org => {
         if(org != currentSite) {
@@ -30,8 +36,17 @@ let querying = browser.tabs.query(queryInfo).then((result) => {
 
     //TODO: APPLY LINKS TO RECOMMENDED ARRAY BASED FROM THE RESULTS OF THE GOOGLE SEARCH
 
-    //TODO: MATCH THE NEWS SITE IMAGES TO THE NEW ARTICLES THAT ARE IN ORGS (BASED FROM GOOGLE SEARCHES)
-    
+    //Match the images with links and display them in correct location
+    document.getElementById("current-news-link").href = result[0].url;
+    document.getElementById("current-news-image").src = dict[currentSite];
+
+    //TODO: Replace links with accurate ones once google results are implemented
+    document.getElementById("recommended-news-first-link").href = result[0].url;
+    document.getElementById("recommended-news-first-image").src = dict[currentSite];
+    document.getElementById("recommended-news-second-link").href = result[0].url;
+    document.getElementById("recommended-news-second-image").src = dict[currentSite];
+
+
 
 });
 
