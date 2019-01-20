312267ec6836f23c3b9f5f4a3ff14159dd89e98d
dylanalvarez1 <dylanalvarez1@ufl.edu>
2019-01-20 05:22:39 -0500

    revise algorithm and request

--- popup/popup.js
+++ popup/popup.js
@@ -4,7 +4,7 @@ let recommended = [];
 const dict = {
     "https://www.foxnews.com": "images/fox.png",
     "https://www.bbc.com": "images/bbc.jpeg",
-    "http://www.msnbc.com": "images/msnbc.png"
+    "https://www.msnbc.com": "images/msnbc.png"
 }
 
 //Query info to get active tabs
@@ -17,7 +17,7 @@ let querying = browser.tabs.query(queryInfo).then((result) => {
 
     //Search google with this title
     console.log("Title: ", result);
-    let searchQuery = result[0].title; 
+    let searchQuery = result[0].title.slice(0, result[0].title.length/2); 
 
     //Figure out which news site this current article is on. Find the current domain, get the recommended domains, apply the links
     let currentSite = result[0].url.match(/^((?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+))/img, "")[0];
@@ -45,13 +45,24 @@ let querying = browser.tabs.query(queryInfo).then((result) => {
     });
     console.log("About to search");
     //TODO: GET GOOGLE SEARCH RESULTS HERE based on searchQuery (only use recommened domains)
+    
+    let urlArray = [];
+    urlArray[0] = `https://api.cognitive.microsoft.com/bing/v7.0/search?q=${searchQuery} site: ${recommended[0]}&count=10&offset=0&mkt=en-us&safesearch=Moderate`
+    urlArray[1] = `https://api.cognitive.microsoft.com/bing/v7.0/search?q=${searchQuery} site: ${recommended[1]}&count=10&offset=0&mkt=en-us&safesearch=Moderate`
+
+    for(let i = 0; i < 2; i++) {
+        requestURL(i, urlArray[i], result, currentSite);
+    }
+
+});
+
+function requestURL(i, url, result, currentSite) {
     let xhttp = new XMLHttpRequest();
-    let url = `https://api.cognitive.microsoft.com/bing/v7.0/search?q=${searchQuery}+"site:"+${recommended[0]}+" OR "+${recommended[1]}+&count=10&offset=0&mkt=en-us&safesearch=Moderate`
     xhttp.onreadystatechange = function() {
         console.log("Before ready");
         if (this.readyState == 4 && this.status == 200) {
             // Typical action to be performed when the document is ready:
-            console.log("results: ", xhttp.responseText);
+            console.log("results: ", JSON.parse(xhttp.responseText));
             let response = JSON.parse(xhttp.responseText);
 
             //bingResults is the domain and the path params from the response, domainResults is just the domain from the response
@@ -59,17 +70,8 @@ let querying = browser.tabs.query(queryInfo).then((result) => {
             let domainResults = [];
 
             bingResults.push(response["webPages"]["value"][0].url);
-            bingResults.push(response["webPages"]["value"][1].url);
             domainResults[0] = bingResults[0].match(/^((?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+))/img, "");
-            domainResults[1] = bingResults[1].match(/^((?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+))/img, "");
 
-            console.log("Test area:")
-            console.log(recommended[0])
-            console.log(recommended[1])
-            console.log(bingResults[0])
-            console.log(bingResults[1])
-            console.log(domainResults[0])
-            console.log(domainResults[1])
             //TODO: APPLY LINKS TO RECOMMENDED ARRAY BASED FROM THE RESULTS OF THE GOOGLE SEARCH
 
             //Match the images with links and display them in correct location
@@ -77,20 +79,22 @@ let querying = browser.tabs.query(queryInfo).then((result) => {
             document.getElementById("current-news-image").src = dict[currentSite];
 
             //TODO: Replace links with accurate ones once google results are implemented
-            document.getElementById("recommended-news-first-link").href = bingResults[0];
-            document.getElementById("recommended-news-first-image").src = dict[domainResults[0]];
-            document.getElementById("recommended-news-second-link").href = bingResults[1].url;
-            document.getElementById("recommended-news-second-image").src = dict[domainResults[1]];
+            if(i == 0) {
+                document.getElementById("recommended-news-first-link").href = bingResults[0];
+                document.getElementById("recommended-news-first-image").src = dict[domainResults[0]];
+            }
+            else if(i == 1) {
+                document.getElementById("recommended-news-second-link").href = bingResults[1].url;
+                document.getElementById("recommended-news-second-image").src = dict[domainResults[1]];
+            }
+            
+            
         }
     };
     xhttp.open("GET", url, true);
     xhttp.setRequestHeader("Ocp-Apim-Subscription-Key", "380ffeed758e4b99be10b3b5fb1cc143");
     xhttp.send();
-    
 
-    
-    
-    
-});
+}
 
 console.log(`Alt-Facts: Was loaded!`);
